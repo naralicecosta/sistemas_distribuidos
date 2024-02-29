618b345ea7b36562ea587fa66b99e442cc473912
naralicecosta <naralicecosta04@gmail.com>
2024-02-29 20:42:40 -0300

    adicionando codigo aula

--- aula2902/Program.cs
+++ aula2902/Program.cs
@@ -1,10 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
 
 class Program 
 {
-    using var htttpClient = new htttpClient();
+    using var htttpClient = new htttpClient(); // variavel genérica
     string url = "https://github.com"
     HttpResponseMessage response = await htttpClient GetAsync(url)
+
+    if (responde.IsSuccessStatusCode)
+    {
+        string respondeBody = await responde.Content.ReadAsStringAsync()
+        Console.WriteLine(respondeBody)
+    }
 
 }
