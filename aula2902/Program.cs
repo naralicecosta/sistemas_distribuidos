7d24e5d34741ea04a2df4e7af1599f9e1d82f62f
naralicecosta <naralicecosta04@gmail.com>
2024-02-29 20:35:57 -0300

    arquivos aula

--- aula2902/Program.cs
+++ aula2902/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+class Program 
+{
+    using var htttpClient = new htttpClient();
+    string url = "https://github.com"
+    HttpResponseMessage response = await htttpClient GetAsync(url)
+
+}
