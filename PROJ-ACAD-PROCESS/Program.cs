2c4b929e4d4b65e95e0c32942ba21e7106656d59
naralicecosta <naralicecosta04@gmail.com>
2024-03-14 20:17:06 -0300

    percorrendo for

--- PROJ-ACAD-PROCESS/Program.cs
+++ PROJ-ACAD-PROCESS/Program.cs
@@ -1,3 +1,24 @@
-﻿Parallel For (0, 10, i=> {
-    Console.WriteLine($"Thead {i} executando.")
-})
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Lista para armazenar os números
+        List<int> numeros = new List<int>();
+
+        // Loop for para adicionar números de 1 a 9 à lista
+        for (int i = 1; i <= 9; i++)
+        {
+            numeros.Add(i);
+        }
+
+        // Imprimindo a lista
+        Console.WriteLine("Lista de números de 1 a 9:");
+        foreach (int numero in numeros)
+        {
+            Console.WriteLine(numero);
+        }
+    }
+}
